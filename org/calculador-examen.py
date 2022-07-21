b91ffa2de03e87bc5924a0d2f41373baa49db991
Manu <m.fuica01@ufromail.cl>
2022-07-21 17:38:24 -0400

    chore: actualizar calculador

--- org/calculador-examen.py
+++ org/calculador-examen.py
@@ -1,4 +1,5 @@
 promedio = input("Su promedio (ejemplo: 3.8): ")
-examen=(4-promedio*0.6)/0.4
-print("Nota requerida en examen:")
+nota_de_aprobacion = 4
+examen=(nota_de_aprobacion-promedio*0.6)/0.4
+print("Nota requerida en examen: ")
 print(examen)
